41acb4c18f71c68ed2ee344ef687c99feaeb0a1f
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-12-02 18:25:54 -0500

    Add part 2 solution

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{self, Read, Write};
+use std::{
+    collections::HashMap,
+    io::{self, Read, Write},
+};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -7,7 +10,7 @@ fn main() -> Result<()> {
     io::stdin().read_to_string(&mut input)?;
 
     part1(&input)?;
-    // part2(&input)?;
+    part2(&input)?;
 
     Ok(())
 }
@@ -35,9 +38,64 @@ fn part1(input: &String) -> Result<()> {
     Ok(())
 }
 
-// fn part2(input: &String) -> Result<()> {
-//     let mut sum: u32 = 0;
-//     for line in input.lines() {}
-//     writeln!(io::stdout(), "Sum: {}", sum)?;
-//     Ok(())
-// }
+fn part2(input: &String) -> Result<u32> {
+    // TODO: Move this out of the function. Make it static?
+    let numbers: HashMap<&str, u32> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("nine", 9),
+        ("eight", 8),
+    ]);
+
+    let mut sum: u32 = 0;
+    for line in input.lines() {
+        let mut found_numbers: Vec<(usize, u32)> = Vec::new();
+        for number in numbers.keys() {
+            let mut found: Vec<(usize, u32)> = line
+                .match_indices(number)
+                .map(|(a, b)| (a, *numbers.get(b).unwrap()))
+                .collect();
+            found_numbers.append(&mut found);
+        }
+        for (idx, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                found_numbers.push((idx, c.to_digit(10).unwrap()))
+            }
+        }
+        found_numbers.sort();
+        sum += found_numbers[0].1 * 10 + found_numbers.last().unwrap().1;
+    }
+    writeln!(io::stdout(), "Sum: {}", sum)?;
+    Ok(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part2_basic() {
+        assert_eq!(part2(&String::from("1two1")).unwrap(), 11);
+    }
+    #[test]
+    fn part2_characters() {
+        assert_eq!(part2(&String::from("two1three")).unwrap(), 23);
+    }
+    #[test]
+    fn part2_mixed() {
+        assert_eq!(part2(&String::from("two1")).unwrap(), 21);
+    }
+    #[test]
+    fn part2_mixed_reversed() {
+        assert_eq!(part2(&String::from("1two")).unwrap(), 12);
+    }
+    #[test]
+    fn part2_repeat_characters() {
+        assert_eq!(part2(&String::from("two1two")).unwrap(), 22);
+    }
+}
